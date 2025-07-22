e38961b29158e671c5386cc54a2a4c087c24b7b4
Pabllo Martins <pabllo.mtzcode@gmail.com>
2025-07-21 22:07:46 -0300

    Refatoração: Simplificação da entrada de dados com prompt e conversão para número

--- introducao_javaScript/2_funcao/script.js
+++ introducao_javaScript/2_funcao/script.js
@@ -6,20 +6,15 @@ console.log(`Olá, ${nome1}! Seja bem-vindo ao nosso site.`);
 
 // 2. Peça dois números e exiba a soma
 
-let num1 = prompt("Digite o primeiro número: ");
-let num2 = prompt("Digite o segundo número: ");
-
-num1 = Number(num1);
-num2 = Number(num2);
-
+let num1 = Number(prompt("Digite o primeiro número: "));
+let num2 = Number(prompt("Digite o segundo número: "));
 let soma = num1 + num2;
 
-// console.log(`A soma de ${num1} e ${num2} é ${soma}.`);
+console.log(`A soma de ${num1} e ${num2} é ${soma}.`);
 
 // 3. Peça a idade e diga se a pessoa é maior de idade
 
-let idade1 = prompt("Digite a sua idade: ");
-idade1 = Number(idade);
+let idade1 = Number(prompt("Digite a sua idade: "));
 
 if (idade1 >= 18) {
   console.log("Você é maior de idade.");
@@ -30,7 +25,7 @@ if (idade1 >= 18) {
 // // 4. Peça o nome e o ano de nascimento, e calcule a idade
 
 let nome = prompt("Digite seu nome: ");
-let anoNascimento = prompt("Digite o ano de nascimento: ");
+let anoNascimento = Number(prompt("Digite o ano de nascimento: "));
 let anoAtual = 2025;
 let idade = anoAtual - anoNascimento;
 
@@ -38,11 +33,8 @@ console.log(`${nome}, você tem ${idade} anos.`);
 
 // 5. Peça dois números e mostre qual é o maior
 
-let num3 = prompt("Digite o primeiro número: ");
-let num4 = prompt("Digite o segundo número: ");
-
-num3 = Number(num3);
-num4 = Number(num4);
+let num3 = Number(prompt("Digite o primeiro número: "));
+let num4 = Number(prompt("Digite o segundo número: "));
 
 if (num3 > num4) {
   console.log(`O número ${num3} é maior que o número ${num4}.`);
@@ -52,12 +44,11 @@ if (num3 > num4) {
 
 // 6. Peça a nota de um aluno e informe se foi aprovado (média >= 7)
 
-let num5 = prompt("Digite a primeira nota: ");
-let num6 = prompt("Digite a segunda nota: ");
-let num7 = prompt("Digite a terceira nota: ");
-let num8 = prompt("e por fim a quarta nota: ");
+let num5 = Number(prompt("Digite a primeira nota: "));
+let num6 = Number(prompt("Digite a segunda nota: "));
+let num7 = Number(prompt("Digite a terceira nota: "));
+let num8 = Number(prompt("e por fim a quarta nota: "));
 media = (num5 + num6 + num7 + num8) / 4;
-num5, num6, num7, num8, media = Number(num5, num6, num7, num8, media);
 
 if (media >= 7) {
   console.log(`Aluno aprovado, Nota final: ${media}`);
@@ -67,8 +58,7 @@ if (media >= 7) {
 
 // 7. Peça um número e mostre se é par ou ímpar
 
-let num = prompt("Digite um número: ");
-num = Number(num);
+let num = NUmber(prompt("Digite um número: "));
 
 if (num % 2 == 0) {
   console.log(`O número ${num} é par`);
@@ -80,10 +70,8 @@ if (num % 2 == 0) {
 // Entrada: número 1 e número 2
 // Saída: resultados das 4 operações
 
-let num9 = prompt("Digite o primeiro número: ");
-let num10 = prompt("Digite o segundo número: ");
-num1 = Number(num9);
-num2 = Number(num10);
+let num9 = Number(prompt("Digite o primeiro número: "));
+let num10 = Number(prompt("Digite o segundo número: "));
 
 console.log(`O resultado entre ${num9} + ${num10} é: `, num9 + num10);
 console.log(`A diferença entre ${num9} - ${num10} é: `, num9 - num10);
@@ -94,12 +82,9 @@ console.log(`O quociente entre ${num9} / ${num10} é: `, num1 / num10);
 // Entrada: distância e tempo
 // Saída: "A velocidade média foi de X km/h"
 
-let distancia = prompt("Informe a distancia percorrida em km: ");
-let tempo = prompt("Informe o tempo percorrido em horas: ");
+let distancia = Number(prompt("Informe a distancia percorrida em km: "));
+let tempo = Number(prompt("Informe o tempo percorrido em horas: "));
 let resultado = distancia / tempo;
-distancia = Number(distancia);
-tempo = Number(tempo);
-resultado = Number(resultado);
 
 console.log(`A velocidade média foi de ${resultado} km/h`);
 
@@ -107,9 +92,14 @@ console.log(`A velocidade média foi de ${resultado} km/h`);
 // Entrada: valor em reais
 // Saída: "R$100 equivalem a $20" (exemplo com cotação R$5.00)
 
-let vlReal = prompt("Informe o valor que deseja converter em R$(Real-BR): ");
+let vlReal = Number(
+  prompt("Informe o valor que deseja converter em R$(Real-BR): ")
+);
 const cotacaoDolar = 5.65;
 let conversao = vlReal / cotacaoDolar;
-vlReal = Number(vlReal);
 
-console.log(`Conversao de R$ para U$: ${conversao.toFixed(2)}`);
+console.log(
+  `R$${vlReal.toFixed(2)} equivalem a $${conversao.toFixed(
+    2
+  )} com a cotação de ${cotacaoDolar}`
+);
---
+++ introducao_javaScript/2_funcao/tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+let num1 = Number(prompt("Digite o primeiro número: "));
+let num2 = Number(prompt("Digite o segundo número: "));
+let soma = num1 + num2;
+
+console.log(`A soma de ${num1} e ${num2} é ${soma}.`);
